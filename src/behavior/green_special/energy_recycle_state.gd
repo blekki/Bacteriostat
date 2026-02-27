bafc8f4fe19abcf0f1a5f8a0801adee3cf345495
Orest Chumak <orest.chumak@gmail.com>
2026-02-27 23:34:54 +0200

    Remove the "EnergyRecycle" state

--- src/behavior/green_special/energy_recycle_state.gd
+++
@@ -1,9 +0,0 @@
-class_name EnergyRecycleState
-extends RefCounted
-
-func update(bacterium: Bacterium):
-	if Singlton.time_season == Enums.TimeSeasons.DAY:
-		bacterium.behavior_state = StateMachine.photosynthesis
-
-func do_task(bacterium: Bacterium):
-	bacterium.recycle_energy()
--- src/behavior/state_machine.gd
+++ src/behavior/state_machine.gd
@@ -2,8 +2,7 @@ class_name StateMachine
 extends RefCounted
 
 static var photosynthesis = PhotosynthesisState.new()
-static var energy_recycle = EnergyRecycleState.new()
 static var shadding = ShaddingState.new()
 
 static func get_start_green_bacterium_state() -> RefCounted:	# todo: set normal returning type
-	return energy_recycle
+	return PhotosynthesisState
