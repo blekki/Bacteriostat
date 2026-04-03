fd3cf24aed368cfea2ad19d1d5211dc64d75b9ea
Orest Chumak <orest.chumak@gmail.com>
2026-04-03 21:10:47 +0300

    [InfoUtils] update - remove unneeded.

--- src/info_utils.gd
+++ src/info_utils.gd
@@ -2,13 +2,11 @@
 class_name InfoUtils
 extends RefCounted
 
-const _NEGATIVE_RESULT: bool = false
-const _POSITIVE_RESULT: bool = true
-const _RAW_INDEX: int = -1
-
 # <> section "the nearest object" <>
 ## Find nearest object with a special condition but if no one is, return empty [InfoPack]
 static func _try_find_nearest_obj(array: Array[InfoPack], condition: Callable) -> InfoPack:
+	const _RAW_INDEX: int = -1
+	
 	if array.size() == 0:
 		return InfoPack.get_empty_pack()
 	
@@ -16,7 +14,7 @@ static func _try_find_nearest_obj(array: Array[InfoPack], condition: Callable) -
 	var index: int = _RAW_INDEX
 	for a in range(0, array.size()):
 		if condition.call(array[a]):
-			if array[a].object.position.length() < nearest:
+			if array[a].object.position.length() < nearest:	# idk: i'm not sure is it correct
 				index = a
 	
 	# return result
@@ -35,7 +33,7 @@ static func get_nearest_pray(array: Array[InfoPack]) -> InfoPack:
 	return _try_find_nearest_obj(array, condition)
 
 ## Find nearest pray but if no one is, return [Enums.ObjectTypes.NONE]
-static func get_nearest_energy_cell(array: Array[InfoPack]) -> InfoPack: # todo: separate "nearest_cell" into "nearest_grass" and "nearest_meat"
+static func get_nearest_energy_cell(array: Array[InfoPack]) -> InfoPack:
 	# prepare condition
 	var condition = func(record: InfoPack):
 		var is_inedible = record.relationship == Enums.RelationshipTypes.INEDIBLE
@@ -45,22 +43,27 @@ static func get_nearest_energy_cell(array: Array[InfoPack]) -> InfoPack: # todo:
 	
 	return _try_find_nearest_obj(array, condition)
 
+## Find nearest predator but if no one is, return [Enums.ObjectTypes.NONE]
+static func get_nearest_predator(array: Array[InfoPack]) -> InfoPack:
+	# prepare condition
+	var condition = func(record: InfoPack):
+		var is_predator = record.relationship == Enums.RelationshipTypes.PREDATOR
+		return is_predator
+	
+	return _try_find_nearest_obj(array, condition)
+
 # <> section "is object nearby" <>
 static func _try_find_obj(array: Array[InfoPack], condition: Callable) -> bool:
+	var is_found: bool = false
 	if array.size() == 0:
-		return _NEGATIVE_RESULT
+		return is_found
 	
-	var index: int = _RAW_INDEX
 	for a in range(0, array.size()):
 		if condition.call(array[a]):
-			index = a
+			is_found = true
 			break	# object is found, loop can be finished
 	
-	# return result
-	if index == _RAW_INDEX:
-		return _POSITIVE_RESULT
-	else:
-		return _NEGATIVE_RESULT
+	return is_found
 
 static func is_lure_nearby(array: Array[InfoPack]) -> bool:
 	# prepare condition
@@ -69,4 +72,11 @@ static func is_lure_nearby(array: Array[InfoPack]) -> bool:
 		var is_edible = record.relationship == Enums.RelationshipTypes.EDIBLE
 		var is_lure = is_inedible or is_edible
 		return is_lure
+	return _try_find_obj(array, condition)
+
+static func is_predator_nearby(array: Array[InfoPack]) -> bool:
+	# prepare condition
+	var condition = func(record: InfoPack):
+		var is_predator = record.relationship == Enums.RelationshipTypes.PREDATOR
+		return is_predator
 	return _try_find_obj(array, condition)
