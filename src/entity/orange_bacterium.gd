c2d0aa77940193ad2d121d84920aafada7d0ed0e
Orest Chumak <orest.chumak@gmail.com>
2026-04-09 14:59:10 +0300

    Rework [OrangeBacterium] dash cooldown

--- src/entity/orange_bacterium.gd
+++ src/entity/orange_bacterium.gd
@@ -2,8 +2,9 @@ class_name OrangeBacterium
 extends Bacterium
 
 var is_power_dash_activated: bool = false
-var power_dash_acceleration: float = 2000
-@onready var cooldown = $ActionCooldown
+var power_dash_acceleration: float = 3000
+@onready var timer = $SmartTimer
+var is_power_dash_active = false
 
 # <> method section <>
 func _ready():
@@ -24,11 +25,9 @@ func _physics_process(delta: float):
 
 func _dash(_acceleration: float):
 	if is_power_dash_activated == true:
-		if cooldown.is_stopped():
-			super(power_dash_acceleration)
-			cooldown.try_cooldown(5, "POWER_DASH_COOLDOWN")
-			return
-	super(acceleration)	# default deceleration
+		super(power_dash_acceleration)
+	else:
+		super(acceleration)	# default dash
 	
 # <> behavior methods section <>
 func hunting():
@@ -44,8 +43,23 @@ func hunting():
 	var prey_record: InfoPack = InfoUtils.get_nearest_prey(nearby_objects)
 	_choice_hunting_action(prey_record)
 
+func dash_target(target: Entity):
+	if is_power_dash_active == true:
+		timer.try_process(2, "POWER_DASH")
+		intercept_target(target.position, target.velocity)
+	else:
+		timer.try_process(1.2, "COOLDOWN")
+		reach_target(target.position)
+
 ## Decide what kind action must be used if nearby environment full of objects.
 func _choice_hunting_action(prey_record: InfoPack):
+	
+	if timer.is_stopped():
+		if is_power_dash_active:
+			is_power_dash_active = false
+		else:
+			is_power_dash_active = true
+	
 	if prey_record.is_not_empty():
 		set_nav_target(prey_record.object.position)
 		
@@ -53,8 +67,6 @@ func _choice_hunting_action(prey_record: InfoPack):
 		if distance < attack_radius:
 			bite_target(prey_record.object)
 		if distance < view_distance:
-			is_power_dash_activated = true
-			intercept_target(get_nav_target(), prey_record.object.velocity)
-			is_power_dash_activated = false
+			dash_target(prey_record.object)
 	else:
 		patrol()	# default
